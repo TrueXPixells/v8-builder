08a37341cfeb8d37e88b1c5174d9dacb66e10cbc
TrueXPixells <68606364+TrueXPixells@users.noreply.github.com>
2023-10-31 21:52:04 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -109,18 +109,11 @@ set +e
 find ~/v8/v8/out.gn/$ARCH ~/v8/v8/out.gn/$ARCH/obj -type f -maxdepth 1 -not -name "*.stamp" -not -name "*.ninja" -not -name "*.json" -not -name "*.TOC"
 cp ~/v8/v8/out.gn/$ARCH/args.gn ~/v8_zip
 cp ~/v8/v8/out.gn/$ARCH/icudtl.dat ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/libv8.so ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/obj/libv8_monolith.a ~/v8_zip
-
-
-cp ~/v8/v8/out.gn/$ARCH/*.dll ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/*.lib ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/*.a ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH/*.so ~/v8_zip
 cp ~/v8/v8/out.gn/$ARCH/*.dylib ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/obj/*.dll ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/obj/*.lib ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH/*.dll ~/v8_zip
 cp ~/v8/v8/out.gn/$ARCH/obj/*.a ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/obj/*.dylib ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH/obj/*.lib ~/v8_zip
 
 # Enable "exit on error"
 set -e
