76e7005eb76e2d2c3557e9a2d7ba0f76ba6f8035
TrueXPixells <68606364+TrueXPixells@users.noreply.github.com>
2023-11-01 17:46:59 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ set -e
 
 VERSION=$1
 REPO=$2
-SHORT_PLATFORM=$3
-SHORT_ARCH=$4
+PLATFORM=$3
+ARCH=$4
 IS_MONOLITHIC_BUILD=$5
 IS_DEBUG=$6
 IOS_DEPLOY_TARGET=$7
 
 # Conf
-if [[ "$SHORT_PLATFORM" == "linux" || "$SHORT_PLATFORM" == "android" ]]; then
+if [[ "$PLATFORM" == "linux" || "$PLATFORM" == "android" ]]; then
     sudo apt-get update -y 1> nul
     sudo apt-get upgrade -y 1> nul
 fi
@@ -24,7 +24,7 @@ git config --global core.filemode false 1> nul
 
 cd ~
 echo "=====[ Getting Depot Tools ]====="
-if [ $SHORT_PLATFORM = "win" ]; then    
+if [ $PLATFORM = "win" ]; then    
     powershell -command "Invoke-WebRequest https://storage.googleapis.com/chrome-infra/depot_tools.zip -O depot_tools.zip" 1> nul
     7z x depot_tools.zip -o* 1> nul
     export DEPOT_TOOLS_WIN_TOOLCHAIN=0
@@ -32,7 +32,7 @@ else
     git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git 1> nul
 fi
 export PATH=$(pwd)/depot_tools:$PATH
-if [ $SHORT_PLATFORM = "win" ]; then  
+if [ $PLATFORM = "win" ]; then  
 gclient
 fi
 # Build
@@ -41,39 +41,39 @@ mkdir v8
 cd v8
 
 echo "=====[ Fetching V8 ]====="
-if [[ $SHORT_PLATFORM == "win" || $SHORT_PLATFORM == "mac" || $SHORT_PLATFORM == "ios" ]]; then  
+if [[ $PLATFORM == "win" || $PLATFORM == "mac" || $PLATFORM == "ios" ]]; then  
 fetch v8
 fi
-if [[ "$SHORT_PLATFORM" == "linux" || "$SHORT_PLATFORM" == "android" ]]; then
+if [[ "$PLATFORM" == "linux" || "$PLATFORM" == "android" ]]; then
 fetch --nohooks v8
 fi
-echo "target_os = ['$SHORT_PLATFORM']" >> .gclient
+echo "target_os = ['$PLATFORM']" >> .gclient
 cd ~/v8/v8
 git checkout $VERSION
 
-if [[ $SHORT_PLATFORM == "win" || "$SHORT_PLATFORM" == "android" ]]; then
+if [[ $PLATFORM == "win" || "$PLATFORM" == "android" ]]; then
 gclient sync
 fi
 
-if [[ "$SHORT_PLATFORM" = "linux" || "$SHORT_PLATFORM" == "android" ]]; then
-if [[ $SHORT_PLATFORM = "linux" ]]; then
+if [[ "$PLATFORM" = "linux" || "$PLATFORM" == "android" ]]; then
+if [[ $PLATFORM = "linux" ]]; then
 ./build/install-build-deps.sh --no-syms --no-nacl --no-prompt
 else
 ./build/install-build-deps.sh --no-syms --no-nacl --no-prompt --android
 fi
-./build/linux/sysroot_scripts/install-sysroot.py --arch=$SHORT_ARCH
+./build/linux/sysroot_scripts/install-sysroot.py --arch=$ARCH
 fi
-if [ $SHORT_PLATFORM != "win" ]; then  
+if [ $PLATFORM != "win" ]; then  
 gclient runhooks
 fi
 
 echo "=====[ Building V8 ]====="
 
 ARGS=""
-if [ $SHORT_PLATFORM = "win" ]; then
+if [ $PLATFORM = "win" ]; then
 ARGS+="is_clang=true use_lld=false"
 fi
-if [ $SHORT_PLATFORM = "ios" ]; then
+if [ $PLATFORM = "ios" ]; then
 ARGS+="v8_enable_pointer_compression=false ios_enable_code_signing=false ios_deployment_target=\"$IOS_DEPLOY_TARGET\""
 fi
 if [ $IS_MONOLITHIC_BUILD = "true" ]; then
@@ -87,13 +87,13 @@ else
 ARGS+=" is_debug=false"
 fi
 sed -i 's/"-Wmissing-field-initializers",/"-Wmissing-field-initializers","-Wctad-maybe-unsupported","-D_SILENCE_ALL_CXX20_DEPRECATION_WARNINGS",/' BUILD.gn
-python ./tools/dev/v8gen.py $SHORT_ARCH.release -vv --no-goma -- $ARGS target_os=\"$SHORT_PLATFORM\" target_cpu=\"$SHORT_ARCH\" v8_target_cpu=\"$SHORT_ARCH\" use_goma=false enable_nacl=false use_custom_libcxx=false v8_enable_sandbox=false v8_enable_i18n_support=true v8_use_external_startup_data=false symbol_level=0
+python ./tools/dev/v8gen.py $ARCH.release -vv --no-goma -- $ARGS target_os=\"$PLATFORM\" target_cpu=\"$ARCH\" v8_target_cpu=\"$ARCH\" use_goma=false enable_nacl=false use_custom_libcxx=false v8_enable_sandbox=false v8_enable_i18n_support=true v8_use_external_startup_data=false symbol_level=0
 
-ninja -C out.gn/$SHORT_ARCH.release -t clean 1> nul
+ninja -C out.gn/$ARCH.release -t clean 1> nul
 if [ $IS_MONOLITHIC_BUILD = "true" ]; then
-ninja -C out.gn/$SHORT_ARCH.release v8_monolith
+ninja -C out.gn/$ARCH.release v8_monolith
 else
-ninja -C out.gn/$SHORT_ARCH.release v8
+ninja -C out.gn/$ARCH.release v8
 fi
 
 # ZIP
@@ -104,19 +104,19 @@ cp -r ~/v8/v8/include ~/v8_zip 1> nul
 
 # Disable "exit on error"
 set +e
-find ~/v8/v8/out.gn/$SHORT_ARCH.release ~/v8/v8/out.gn/$SHORT_ARCH.release/obj -type f -maxdepth 1 -not -name "*.stamp" -not -name "*.ninja" -not -name "*.json" -not -name "*.TOC"
-cp ~/v8/v8/out.gn/$SHORT_ARCH.release/args.gn ~/v8_zip
-cp ~/v8/v8/out.gn/$SHORT_ARCH.release/icudtl.dat ~/v8_zip
-cp ~/v8/v8/out.gn/$SHORT_ARCH.release/*.so ~/v8_zip
-cp ~/v8/v8/out.gn/$SHORT_ARCH.release/*.dylib ~/v8_zip
-cp ~/v8/v8/out.gn/$SHORT_ARCH.release/*.dll ~/v8_zip
-cp ~/v8/v8/out.gn/$SHORT_ARCH.release/obj/*.a ~/v8_zip
-cp ~/v8/v8/out.gn/$SHORT_ARCH.release/obj/*.lib ~/v8_zip
+find ~/v8/v8/out.gn/$ARCH.release ~/v8/v8/out.gn/$ARCH.release/obj -type f -maxdepth 1 -not -name "*.stamp" -not -name "*.ninja" -not -name "*.json" -not -name "*.TOC"
+cp ~/v8/v8/out.gn/$ARCH.release/args.gn ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH.release/icudtl.dat ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH.release/*.so ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH.release/*.dylib ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH.release/*.dll ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH.release/obj/*.a ~/v8_zip
+cp ~/v8/v8/out.gn/$ARCH.release/obj/*.lib ~/v8_zip
 
 # Enable "exit on error"
 set -e
 
-if [ $SHORT_PLATFORM != "win" ]; then
+if [ $PLATFORM != "win" ]; then
 zip -r v8.zip ~/v8_zip/* 1> nul
 else
 7z a v8.zip ~/v8_zip 1> nul
